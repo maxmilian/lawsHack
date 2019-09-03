27f99f6e0d2e409a9a726f5d6d225f8cee422e8b
maxmilian <maxmilian@gmail.com>
2019-09-03 11:22:12 +0800

    add column JCITATION

--- citation.py
+++ citation.py
@@ -10,12 +10,14 @@ MONGO_HOST = '13.113.158.197:37017'
 FILENAME = './files/years.txt'
 FILENAME_OK = 'years.ok.txt'
 
+RE_CITATION_1 = r"(最高|高等|行政)法院(著有)?[○００一二三四五六七八九十0123456789]+年度?\S{1,3}字第[○００一二三四五六七八九十0123456789]+號?(民事|刑事)?(、[○０一二三四五六七八九十0123456789]+年\S{1,3}字第[○０一二三四五六七八九十0123456789]+號)*(著有)?(判例|判決|裁判)?(可資|足資|意旨|要旨)?參照"
+RE_CITATION_2 = r"參照(最高|高等|行政)法院(著有)?[○００一二三四五六七八九十0123456789]+年度?\S{1,3}字第[○００一二三四五六七八九十0123456789]+號?(民事|刑事)?(、[○０一二三四五六七八九十0123456789]+年\S{1,3}字第[○０一二三四五六七八九十0123456789]+號)*(著有)?(判例|判決|裁判)?(可資|足資|意旨|要旨)?"
+
 def get_content_from_mongo(id):
     conn = MongoClient(MONGO_HOST)
     db = conn.TW_case
     case = db.TW_case.find_one({"_id": id}, {"JFULL": 1})
     print(case)
-
 
 def get_year_from_mongo(year):
     conn = MongoClient(MONGO_HOST)
@@ -32,9 +34,17 @@ def get_year_from_mongo(year):
             id = case['_id']
             content = case['JFULL']
             if "參照" in content or "參考" in content:
-                with open("ids.txt", 'a+') as f:
-                    f.write(id + "\n")
-                    f.close()
+                content = re.sub(r'\r?\n', '', content)
+                x = re.search(RE_CITATION_1, content)
+                if x is None:
+                    x = re.search(RE_CITATION_2, content)
+
+                if x is not None:
+                    citation = x.group()
+                    print(id + "\n" + citation)
+                    db.TW_case.find_one_and_update({'_id': id}, {'$set': {'JCITATION': citation}})
+                else:
+                    print(id + "\nno refer")
 
         skip += limit
 
@@ -49,7 +59,6 @@ def load_files(filename):
     return lists
 
 if __name__== "__main__":
-    print('main')
 
     todo_file = FILENAME
     ok_file = FILENAME_OK
